11b1336925cea45402438ec370bb4153a8c665e4
Seno Wijayanto <senowijayanto@gmail.com>
2019-07-31 06:20:08 +0700

    AsyncAwait

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log('Starting')
+
+setTimeout(() => {
+    console.log('After 2 Second')
+}, 2000)
+
+setTimeout(() => {
+    console.log('After 0 Second')
+}, 0)
+
+console.log('Stopping')
